3956366f28ca215a957b3eb5ee5d419ce950f6c2
Deepa Jaganathan <deepa2y5@gmail.com>
2022-02-16 10:50:19 -0500

    updated-port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 var users = require('./routes/users');
 var states = require('./routes/states');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 app.use('/users', users);
